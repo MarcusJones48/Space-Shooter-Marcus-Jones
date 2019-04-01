4b57247956b5c265c9f95ebd642ba34282f83aa7
Marcus Jones <marcusjones42@knights.ucf.edu>
2019-03-31 23:35:12 -0400

    Adding to Project

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float lifetime;
+
+    void Start ()
+    {
+        Destroy (gameObject, lifetime); 
+    }
+}
